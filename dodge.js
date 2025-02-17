b8e2295d8f3fd253ec2d542bbf5e9f19a9785f13
waliddaoud-code <daoud.walid.2002@gmail.com>
2025-02-17 17:44:54 +0100

    Update dodge.js

--- dodge.js
+++ dodge.js
@@ -2,11 +2,12 @@ const button = document.getElementById('btn')
 const ddg = document.getElementById('dodge')
 let num = document.getElementById('num')
 const rick = new Audio("https://od.lk/s/NzhfNjY1MTE5OTJf/poussin.mp3")
-
+const farfadet = new Audio("https://od.lk/s/NzhfNjY1MTE3MzZf/Le%20Farfadet%20Malicieux.mp3")
+const poussin = document.querySelector('.move')
 let html = '';
 
 
-
+farfadet.loop = true
 
 
 button.addEventListener('click', () => {
@@ -14,5 +15,9 @@ button.addEventListener('click', () => {
   rick.currentTime = 0
   num.innerHTML++
   rick.play()
+  farfadet.play()
+
+  poussin.classList.remove('move')
+  poussin.classList.add('move1')
 
 })
